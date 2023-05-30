da41c76a5b23d8bdf0c73eeb73e962e779c9430a
yan <sdeyanchen@gmail.com>
2023-05-30 16:20:59 -0400

    transit stop api

--- src/main/java/com/yan/springbootlambda/controller/TransitTimeController.java
+++ src/main/java/com/yan/springbootlambda/controller/TransitTimeController.java
@@ -4,6 +4,8 @@ import com.yan.springbootlambda.exception.TransitClientException;
 import com.yan.springbootlambda.model.Response;
 import com.yan.springbootlambda.service.TransitService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,6 +19,12 @@ public class TransitTimeController {
     public Response transitTime() throws TransitClientException {
         String resp = transitService.fetchTransitTime();
         return new Response(true, resp);
+    }
+
+    @RequestMapping(value = "/stops", method = RequestMethod.GET)
+    public ResponseEntity<Response> stops() {
+        String resp = transitService.fetchTransitStops().values().toString();
+        return new ResponseEntity<>(new Response(true, resp), HttpStatus.OK);
     }
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
--- src/main/java/com/yan/springbootlambda/service/TransitService.java
+++ src/main/java/com/yan/springbootlambda/service/TransitService.java
@@ -5,6 +5,12 @@ import com.yan.springbootlambda.exception.TransitClientException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 @Service
 public class TransitService {
     @Autowired
@@ -13,4 +19,25 @@ public class TransitService {
     public String fetchTransitTime() throws TransitClientException {
         return transitClient.fetchTransitSchedule();
     }
+
+    public Map<String, String> fetchTransitStops() {
+        Map<String, String> stops = new HashMap<>();
+        BufferedReader reader;
+        String line;
+
+        try {
+            reader = new BufferedReader(new FileReader("src/main/resources/stops.txt"));
+            line = reader.readLine();
+            while (line != null) {
+                System.out.println(line);
+                var words = line.split(",");
+                stops.put(words[0], words[2]);
+                line = reader.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return stops;
+    }
+
 }
