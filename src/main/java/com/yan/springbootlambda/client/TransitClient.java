7373a4c475a2b4322e558d9adcc461d9b9fc05c0
yan <sdeyanchen@gmail.com>
2023-06-08 15:31:14 -0400

    add support for multiple mta line

--- src/main/java/com/yan/springbootlambda/client/TransitClient.java
+++ src/main/java/com/yan/springbootlambda/client/TransitClient.java
@@ -1,26 +1,31 @@
 package com.yan.springbootlambda.client;
 
+import com.google.protobuf.InvalidProtocolBufferException;
 import com.google.transit.realtime.GtfsRealtime;
 import com.yan.springbootlambda.exception.TransitClientException;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.ByteBuffer;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
 public class TransitClient {
 
-    public static final String MTA_TRANSIT_URL = "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-bdfm";
+    public static final String MTA_TRANSIT_BASE_URL = "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2F";
     public static final String MTA_TRANSIT_API_KEY = "x4jPVKUJ8K2bq8jSyhoIf4bA0HiwAR939fcUXTUZ";
 
-    public List<GtfsRealtime.TripUpdate.StopTimeUpdate> fetchTransitSchedule() throws TransitClientException {
+    public List<GtfsRealtime.TripUpdate.StopTimeUpdate> fetchTransitSchedule(String stationId) throws TransitClientException {
         URL url;
         try {
             System.out.println("---fetch new time with api---");
-            url = new URL(MTA_TRANSIT_URL);
+            url = new URL(MTA_TRANSIT_BASE_URL + urlPath(stationId));
             HttpURLConnection myURLConnection = (HttpURLConnection) url.openConnection();
             myURLConnection.setRequestProperty("X-API-KEY", MTA_TRANSIT_API_KEY);
             GtfsRealtime.FeedMessage feed = GtfsRealtime.FeedMessage.parseFrom(myURLConnection.getInputStream());
@@ -35,7 +40,17 @@ public class TransitClient {
         }
     }
 
-
-
-
+    private String urlPath(String stationId) {
+        return switch (stationId.toLowerCase().charAt(0)) {
+            case 'a', 'c', 'e' -> "gtfs-ace";
+            case 'b', 'd', 'f', 'm' -> "gtfs-bdfm";
+            case 'g' -> "gtfs-g";
+            case 'j', 'z' -> "gtfs-jz";
+            case 'n', 'q', 'r', 'w' -> "gtfs-nqrw";
+            case 'l' -> "gtfs-l";
+            case '1', '2', '3', '4', '5', '6', '7' -> "gtfs";
+            case 's' -> "gtfs-si";
+            default -> throw new IllegalArgumentException("Unsupported stationId");
+        };
+    }
 }
--- src/main/java/com/yan/springbootlambda/service/Component.java
+++ src/main/java/com/yan/springbootlambda/service/Component.java
@@ -1,32 +1,20 @@
 package com.yan.springbootlambda.service;
 
+import com.yan.springbootlambda.model.TransitData;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.web.context.WebApplicationContext;
 
-import java.time.Instant;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Configuration
 public class Component {
 
-    public static final int CACHE_SIZE = 10;
-    public static final long ZERO = 0L;
-
     @Bean
     @Scope(value = WebApplicationContext.SCOPE_APPLICATION, proxyMode = ScopedProxyMode.TARGET_CLASS)
-    public Map<Instant, List<String>> transitTimeCache() {
-        var cache = new LinkedHashMap<Instant, List<String>>(CACHE_SIZE) {
-            protected boolean removeEldestEntry(Map.Entry<Instant, List<String>> eldest) {
-                return size() > CACHE_SIZE;
-            }
-        };
-        cache.put(Instant.ofEpochMilli(ZERO), new ArrayList<>());
-        return cache;
+    public Map<String, TransitData> transitTimeCache() {
+        return new HashMap<>();
     }
 }
