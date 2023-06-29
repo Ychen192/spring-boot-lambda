8dbb9499f0c9dddb709e91d9db7f42a7d215063b
yan <sdeyanchen@gmail.com>
2023-06-29 10:49:23 -0400

    add cross origin to response headers, default cross origin cross origin is not working

--- src/main/java/com/yan/springbootlambda/controller/TransitTimeController.java
+++ src/main/java/com/yan/springbootlambda/controller/TransitTimeController.java
@@ -3,8 +3,8 @@ package com.yan.springbootlambda.controller;
 import com.yan.springbootlambda.exception.TransitClientException;
 import com.yan.springbootlambda.model.TrainStation;
 import com.yan.springbootlambda.service.TransitService;
-import io.netty.util.internal.StringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,18 +22,23 @@ public class TransitTimeController {
     }
 
     @RequestMapping(value = "/train/time/{trainLine}/{stationId}", method = RequestMethod.GET)
-    @ResponseBody
     public ResponseEntity<Object> transitTime(
             @PathVariable Character trainLine,
             @PathVariable String stationId) throws TransitClientException {
         String station = stationId.substring(0, stationId.length() - 1);
         var resp = transitService.fetchTransitTime(trainLine, station);
-        return new ResponseEntity<>(resp, HttpStatus.OK);
+        return new ResponseEntity<>(resp, responseHeaders(), HttpStatus.OK);
     }
 
     @RequestMapping(value = "/stops/{line}", method = RequestMethod.GET)
     public ResponseEntity<List<TrainStation>> stops(@PathVariable Character line) throws TransitClientException {
         var resp = transitService.fetchTransitStops(line);
-        return new ResponseEntity<>(resp, HttpStatus.OK);
+        return new ResponseEntity<>(resp, responseHeaders(), HttpStatus.OK);
+    }
+
+    private HttpHeaders responseHeaders() {
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set("Access-Control-Allow-Origin", "*");
+        return responseHeaders;
     }
 }
--- src/main/java/com/yan/springbootlambda/service/Component.java
+++ src/main/java/com/yan/springbootlambda/service/Component.java
@@ -3,9 +3,6 @@ package com.yan.springbootlambda.service;
 import com.yan.springbootlambda.model.TransitData;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -33,15 +30,5 @@ public class Component {
             e.printStackTrace();
         }
         return transitStops;
-    }
-
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://yan-static-content.s3-website-us-east-1.amazonaws.com"));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
     }
 }
